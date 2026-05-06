440d0263689eadf4a8a48d2eef3a9a2092c67c3d
mbezitech <innocentfrancismhina@gmail.com>
2026-05-06 09:45:25 +0300

    Show 'Check your internet connection' on SocketException

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import '../utils/constants.dart';
 
@@ -25,55 +26,75 @@ class ApiService {
   }
 
   Future<Map<String, dynamic>> post(String endpoint, Map<String, dynamic> body) async {
-    final response = await http.post(
-      Uri.parse('${AppConstants.apiBaseUrl}$endpoint'),
-      headers: _headers,
-      body: jsonEncode(body),
-    );
-    return _handleResponse(response);
+    try {
+      final response = await http.post(
+        Uri.parse('${AppConstants.apiBaseUrl}$endpoint'),
+        headers: _headers,
+        body: jsonEncode(body),
+      );
+      return _handleResponse(response);
+    } on SocketException {
+      throw ApiException(message: 'Check your internet connection', statusCode: 0);
+    }
   }
 
   Future<Map<String, dynamic>> get(String endpoint, {Map<String, String>? queryParams}) async {
-    final uri = Uri.parse('${AppConstants.apiBaseUrl}$endpoint').replace(queryParameters: queryParams);
-    final response = await http.get(uri, headers: _headers);
-    return _handleResponse(response);
+    try {
+      final uri = Uri.parse('${AppConstants.apiBaseUrl}$endpoint').replace(queryParameters: queryParams);
+      final response = await http.get(uri, headers: _headers);
+      return _handleResponse(response);
+    } on SocketException {
+      throw ApiException(message: 'Check your internet connection', statusCode: 0);
+    }
   }
 
   Future<Map<String, dynamic>> put(String endpoint, Map<String, dynamic> body) async {
-    final response = await http.put(
-      Uri.parse('${AppConstants.apiBaseUrl}$endpoint'),
-      headers: _headers,
-      body: jsonEncode(body),
-    );
-    return _handleResponse(response);
+    try {
+      final response = await http.put(
+        Uri.parse('${AppConstants.apiBaseUrl}$endpoint'),
+        headers: _headers,
+        body: jsonEncode(body),
+      );
+      return _handleResponse(response);
+    } on SocketException {
+      throw ApiException(message: 'Check your internet connection', statusCode: 0);
+    }
   }
 
   Future<Map<String, dynamic>> delete(String endpoint) async {
-    final response = await http.delete(
-      Uri.parse('${AppConstants.apiBaseUrl}$endpoint'),
-      headers: _headers,
-    );
-    return _handleResponse(response);
+    try {
+      final response = await http.delete(
+        Uri.parse('${AppConstants.apiBaseUrl}$endpoint'),
+        headers: _headers,
+      );
+      return _handleResponse(response);
+    } on SocketException {
+      throw ApiException(message: 'Check your internet connection', statusCode: 0);
+    }
   }
 
   Future<Map<String, dynamic>> uploadDocument(String endpoint, Map<String, String> fields, String filePath) async {
-    final uri = Uri.parse('${AppConstants.apiBaseUrl}$endpoint');
-    final request = http.MultipartRequest('POST', uri);
+    try {
+      final uri = Uri.parse('${AppConstants.apiBaseUrl}$endpoint');
+      final request = http.MultipartRequest('POST', uri);
 
-    request.headers.addAll({
-      'Authorization': 'Bearer $_token',
-      'Accept': 'application/json',
-    });
+      request.headers.addAll({
+        'Authorization': 'Bearer $_token',
+        'Accept': 'application/json',
+      });
 
-    fields.forEach((key, value) {
-      request.fields[key] = value;
-    });
+      fields.forEach((key, value) {
+        request.fields[key] = value;
+      });
 
-    request.files.add(await http.MultipartFile.fromPath('document', filePath));
+      request.files.add(await http.MultipartFile.fromPath('document', filePath));
 
-    final streamedResponse = await request.send();
-    final response = await http.Response.fromStream(streamedResponse);
-    return _handleResponse(response);
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+      return _handleResponse(response);
+    } on SocketException {
+      throw ApiException(message: 'Check your internet connection', statusCode: 0);
+    }
   }
 
   Map<String, dynamic> _handleResponse(http.Response response) {
